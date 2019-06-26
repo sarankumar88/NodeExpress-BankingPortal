c0ae4e3fdfc57659dcf2886122021472e12dadba
Palivela <saran.palivela@sap.com>
2019-06-26 17:30:17 +0530

    4thModule

--- src/app.js
+++ src/app.js
@@ -1,23 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
-
 const express = require('express');
 
 const app = express();
-
+const { accounts, users, writeJSON } = require('./data');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({extended:true}))
-
-const accountData = fs.readFileSync(path.join(__dirname,"json","accounts.json"),"utf-8")
-const accounts = JSON.parse(accountData)
-
-const userData = fs.readFileSync(path.join(__dirname,"json","users.json"), "utf-8")
-const users = JSON.parse(userData)
 
 app.get("/" , (req,res) => {
     res.render('index' , {title: "Account Summary", accounts: accounts});
@@ -39,8 +32,7 @@ app.get('/transfer' , (req,res) => res.render('transfer'));
 app.post('/transfer' , (req,res) =>{
     accounts[req.body.from].balance = parseInt(accounts[req.body.from].balance) - parseInt(req.body.amount);
     accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount , 10);
-    const accountsJSON = JSON.stringify(accounts,null,4);
-    fs.writeFileSync(path.join(__dirname,"json/accounts.json") , accountsJSON , 'utf-8');
+    writeJSON();
     res.render('transfer' , {message: "Transfer Completed"});
 });
 
@@ -48,8 +40,7 @@ app.get('/payment', (req,res) => res.render('payment' , {account: accounts.credi
 app.post('/payment', (req, res) => {
     accounts.credit.balance -=req.body.amount;
     accounts.credit.available += parseInt(req.body.amount);
-    const accountsJSON = JSON.stringify(accounts,null,4);
-    fs.writeFileSync(path.join(__dirname,"json", "accounts.json"),accountsJSON,'utf-8');
+    writeJSON();
     res.render('payment', {message: "Payment Successful" , account: accounts.credit});
 
 })
