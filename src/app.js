12da56983f8a832983d12dafd38146cc4e37b1a6
Palivela <saran.palivela@sap.com>
2019-06-26 17:48:00 +0530

    5thModule

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
+const accountRoutes = require('./routes/accounts');
+const servicesRouters = require('./routes/services');
+
 
 const app = express();
 const { accounts, users, writeJSON } = require('./data');
@@ -11,39 +14,13 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({extended:true}))
+app.use('/account',accountRoutes);
+app.use('/services', servicesRouters);
 
 app.get("/" , (req,res) => {
     res.render('index' , {title: "Account Summary", accounts: accounts});
 });
 
-app.get('/savings', (req,res)  => {
-    res.render('account', {account: accounts.savings} )
-});
-
-app.get('/checking', (req,res) => {
-    res.render('account', {account: accounts.checking})
-});
-
-app.get('/credit', (req,res) => {
-    res.render('account', {account: accounts.credit})
-});
-
-app.get('/transfer' , (req,res) => res.render('transfer'));
-app.post('/transfer' , (req,res) =>{
-    accounts[req.body.from].balance = parseInt(accounts[req.body.from].balance) - parseInt(req.body.amount);
-    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount , 10);
-    writeJSON();
-    res.render('transfer' , {message: "Transfer Completed"});
-});
-
-app.get('/payment', (req,res) => res.render('payment' , {account: accounts.credit}));
-app.post('/payment', (req, res) => {
-    accounts.credit.balance -=req.body.amount;
-    accounts.credit.available += parseInt(req.body.amount);
-    writeJSON();
-    res.render('payment', {message: "Payment Successful" , account: accounts.credit});
-
-})
 app.get('/profile' , (req,res) => {
     res.render('profile' , {user: users[0]})
 });
